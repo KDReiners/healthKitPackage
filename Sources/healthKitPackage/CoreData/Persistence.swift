bded11ae37cac1021922588d752a91430cdd0de7
KDReiners <kdreiners@gmx.de>
2022-03-01 21:56:46 +0100

    Bundle

--- Sources/healthKitPackage/CoreData/Persistence.swift
+++ Sources/healthKitPackage/CoreData/Persistence.swift
@@ -11,9 +11,8 @@ public class PersistenceController {
     
     static let shared = PersistenceController()
     static var preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
+        let result = PersistenceController()
         let viewContext = result.cloudContainer.viewContext
-        let shamanContext = result.localContainer.viewContext
         do {
             try viewContext.save()
         } catch {
@@ -26,15 +25,14 @@ public class PersistenceController {
     }()
 
     let cloudContainer: NSPersistentCloudKitContainer
-    let localContainer: NSPersistentContainer
     
-    init(inMemory: Bool = false) {
-        cloudContainer = NSPersistentCloudKitContainer(name: "healthKitConnector")
-        localContainer = NSPersistentContainer(name: "shaman")
-        if inMemory {
-            cloudContainer.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-            localContainer.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        }
+    init() {
+        let bundle = Bundle.module
+         let modelURL = bundle.url(forResource: "healthKitConnector", withExtension: ".momd")!
+         let model = NSManagedObjectModel(contentsOf: modelURL)!
+         cloudContainer = NSPersistentCloudKitContainer(name: "healthKitConnector", managedObjectModel: model)
+        
+//        cloudContainer = NSPersistentCloudKitContainer(name: "healthKitConnector")
         cloudContainer.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
@@ -50,23 +48,6 @@ public class PersistenceController {
                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
-        })
-        localContainer.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                Typical reasons for an error here include:
-                * The parent directory does not exist, cannot be created, or disallows writing.
-                * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                * The device is out of space.
-                * The store could not be migrated to the current model version.
-                Check the error message to determine what the actual problem was.
-                */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-            
         })
         cloudContainer.viewContext.automaticallyMergesChangesFromParent = true
         cloudContainer.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
