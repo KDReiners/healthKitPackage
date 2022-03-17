6e82430d7465971ec8d4b2c7d230da393b4f7050
KDReiners <kdreiners@gmx.de>
2022-03-17 18:11:39 +0100

    centralized extensions

---
+++ Sources/healthKitPackage/Extensions/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Klaus-Dieter Reiners on 17.03.22.
+//
+
+import Foundation
+extension Date {
+    init(_ dateString:String) {
+        let dateStringFormatter = DateFormatter()
+        dateStringFormatter.dateFormat = "yyyy-MM-dd"
+        dateStringFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX") as Locale
+        let date = dateStringFormatter.date(from: dateString)!
+        self.init(timeInterval:0, since:date)
+    }
+}
