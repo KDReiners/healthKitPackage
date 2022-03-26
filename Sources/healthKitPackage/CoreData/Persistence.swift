0b4c96add2ef4e71a693d9f35861cf85d21a7af4
KDReiners <kdreiners@gmx.de>
2022-03-26 20:39:35 +0100

    Continue

--- Sources/healthKitPackage/CoreData/Persistence.swift
+++ Sources/healthKitPackage/CoreData/Persistence.swift
@@ -43,11 +43,12 @@ public struct PersistenceController {
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
-#if os(watchOS)
-        container.persistentStoreDescriptions.first!.configuration = "Watch"
-#endif
 
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+#if os(watchOS)
+            storeDescription.configuration = "Watch"
+#endif
+            
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
