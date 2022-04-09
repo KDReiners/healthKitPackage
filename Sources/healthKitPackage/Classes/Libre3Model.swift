23acc97bb56cd76bc228f6e4b09ccba5cf0f6a05
KDReiners <kdreiners@gmx.de>
2022-04-09 18:07:20 +0200

    Added libre3Model

---
+++ Sources/healthKitPackage/Classes/Libre3Model.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by Klaus-Dieter Reiners on 09.04.22.
+//
+
+import Foundation
+import Foundation
+public class Libre3Model: Model<Libre3primanota> {
+    @Published var result: [Libre3primanota]!
+    public init() {
+        let readOnlyFields: [String] = []
+        super.init(readOnlyFields: readOnlyFields)
+    }
+    override public var items: [Libre3primanota] {
+        get {
+            return result
+        }
+        set
+        {
+            result = newValue.sorted(by: { $1.devicetimestamp ?? Date() < $0.devicetimestamp ?? Date()})
+        }
+    }
+}
