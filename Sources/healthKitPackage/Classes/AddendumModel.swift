d65a9fd8b43047f20ba327a11c3aa468ff5bc61b
KDReiners <kdreiners@gmx.de>
2022-04-10 16:46:32 +0200

    continue

--- Sources/healthKitPackage/Classes/AddendumModel.swift
+++ Sources/healthKitPackage/Classes/AddendumModel.swift
@@ -19,7 +19,7 @@ public class AddendumModel: Model<Addendum> {
         }
         set
         {
-            result = newValue.sorted(by: { $1.timestamp ?? Date() < $0.timestamp ?? Date()})
+            result = newValue.sorted(by: { $1.timestamp ?? Date() > $0.timestamp ?? Date()})
         }
     }
     public func getAttachedLibreLogs() {
