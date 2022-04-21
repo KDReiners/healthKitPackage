53186dc12520f4f00c32e172e0d55dc8a92f6276
KDReiners <kdreiners@gmx.de>
2022-04-21 19:41:07 +0200

    Continue

--- Sources/healthKitPackage/Classes/AddendumTimeCapsules.swift
+++ Sources/healthKitPackage/Classes/AddendumTimeCapsules.swift
@@ -9,6 +9,6 @@ import Foundation
 public class AddendumTimeCapsules: TimeCapsule<Addendum> {
     public typealias T = Addendum
     public init(resolution: Double) {
-        super.init(resolution: resolution, logKey: "timeStamp", model: AddendumModel())
+        super.init(resolution: resolution, logKey: "timestamp", model: AddendumModel())
     }
 }
