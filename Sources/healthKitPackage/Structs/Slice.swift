43d0b92f377091a8a43315f3a7e0af6a2fbb7581
KDReiners <kdreiners@gmx.de>
2022-04-24 21:15:11 +0200

    continue

---
+++ Sources/healthKitPackage/Structs/Slice.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Klaus-Dieter Reiners on 24.04.22.
+//
+
+import Foundation
+public struct Slice {
+    public var quantityType: String
+    public var source: String
+    public var device: String
+    public var queryDateInterval: DateInterval
+    public var sliceDateInterval: DateInterval
+    public var logDate: Date
+    public var value: Any
+}
