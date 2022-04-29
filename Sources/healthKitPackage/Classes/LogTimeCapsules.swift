9a2e20f2c1b17981c6bbbacf4461f4e78156c02b
KDReiners <kdreiners@gmx.de>
2022-04-29 18:55:33 +0200

    Continue

--- Sources/healthKitPackage/Classes/LogTimeCapsules.swift
+++ Sources/healthKitPackage/Classes/LogTimeCapsules.swift
@@ -15,10 +15,8 @@ public class LogTimeCapsules: TimeCapsule<Log> {
         load()
     }
     private func load() {
-        model.items.forEach { log in
-            super.load(logKey: "timeStamp", valueKey:"value" , device: "healthKit", quantityTypeKeyPath: "log2quantitytype", items: model.items)
-            super.slicer()
-        }
+        super.load(logKey: "timeStamp", valueKey:"value" , device: "healthKit", quantityTypeKeyPath: "log2quantitytype", items: model.items)
+        super.slicer()
     }
 }
 
